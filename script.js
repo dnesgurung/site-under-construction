e01e37355ef69254bf85f275e0fe73d2af9ed36a
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2026-02-28 16:49:40 +0000

    script added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const form = document.getElementById('notifyForm');
+    const thankYouMessage = document.getElementById('thankYouMessage')
+
+    
+})
